54a1df0c5b99248b748b836653e5f4d00a5d4169
Joanna Carle <joannadecarle005@gmail.com>
2026-06-16 23:08:48 -0300

    substituindo para o formato json
    Reorganizei o código para utilizar JSON para o armazenamento de dados, ao invés de usar um arquivo txt. adicionei confirmações para as validações

--- AV1-3DAW-COMJS/excluirperguntas.php
+++ AV1-3DAW-COMJS/excluirperguntas.php
@@ -1,17 +1,22 @@
 <?php
 session_start();
-if (!isset($_SESSION["logado"])) header("Location: login.php");
-
-$id = $_GET["id"];
-$linhas = file("perguntas.txt", FILE_IGNORE_NEW_LINES);
-
-$novas = [];
-
-foreach ($linhas as $linha) {
-    list($pid) = explode("|", $linha);
-    if ($pid != $id) $novas[] = $linha;
+if (!isset($_SESSION["logado"])) {
+    header("Location: login.php");
+    exit();
 }
 
-file_put_contents("perguntas.txt", implode("\n", $novas));
+$id = isset($_GET["id"]) ? (int)$_GET["id"] : null;
+$jsonFile = "perguntas.json";
+
+if ($id && file_exists($jsonFile)) {
+    $perguntas = json_decode(file_get_contents($jsonFile), true);
+
+    $novasPerguntas = array_filter($perguntas, function($p) use ($id) {
+        return $p['id'] !== $id;
+    });
+
+    file_put_contents($jsonFile, json_encode(array_values($novasPerguntas), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
 
 header("Location: index.php");
+exit();
